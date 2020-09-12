150e7b423a2b7d9fad53d1ceec582fd424532023
Jakub Pietkun <jakub.pietkun@lingarogroup.com>
2020-09-12 13:16:23 +0200

    table basic example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatTableModule } from "@angular/material/table";
+import { CdkTableModule } from "@angular/cdk/table";
 
-import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { AppComponent } from "./app.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { TableBasicExample } from "./table-basic-example/table-basic-example.component";
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    MatTableModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
+      { path: "", component: ProductListComponent },
+      //{ path: "table-basic-example", component: TableBasicExample },
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent
+    ProductListComponent,
+    TableBasicExample
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
-
+export class AppModule {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
